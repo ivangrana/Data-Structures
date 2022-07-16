8784f6187aed99da8b75f58aae9bc5d584f348e8
ivangrana <ivangrana1956@gmail.com>
2022-07-15 22:51:15 -0300

    .

---
+++ non-linear data structure/binary_tree.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.r_child = None
+        self.l_child = None
+
+class tree():
+    def __init__(self):
+        self.root_node = None
+        
+    def insert(self,data):
+        node = Node(data)
+        if self.root_node is None:
+            self.root_node = node
+        else:
+            current = self.root_node
+            parent = None
+        while True:
+            parent = current
+            if node.data < current.data:
+                current = current.l_child
+                if current is None:
+                    parent.l_child = Node
+                    return
+            else:
+                current = current.r_child
+                if current is None:
+                    parent.r_child = node
+                    return
