00a225f98a2d092b754fc54c03b15f17a77da34f
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2023-08-10 13:16:12 -0300

    Create list.c

---
+++ Linear data structures/List/list.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a linked list node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a new node at the beginning of the linked list
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Function to print the linked list
+void printLinkedList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to free the memory used by the linked list
+void freeLinkedList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtBeginning(&head, 3);
+    insertAtBeginning(&head, 7);
+    insertAtBeginning(&head, 1);
+
+    printf("Linked List: ");
+    printLinkedList(head);
+
+    freeLinkedList(head);
+
+    return 0;
+}
